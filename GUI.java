70540dadb8ecb0cdaaf7827c45dc024b9e2c43e8
JBVidon <jeanbaptiste.vidon@gmail.com>
2022-12-13 11:01:42 -0800

    added a grid

--- GUI.java
+++ GUI.java
@@ -8,20 +8,27 @@ public class GUI extends Main {
     JFrame frame1, frameTimes;//create frame varaible
     JPanel panel1, panelTimes;//create panel variable
     JTextArea instructions;//create instruction variable
+    JTextArea escapetitle;//create instruction variable
     JTextField input;//create input variable
     JButton submit;
     JComboBox<String> openTimes;
+
 
     //constructor
     GUI(){
     }
     public void createScheduleGUI() {
-        frame1 = new JFrame("Escape Room Reservation");//this is the window
+        frame1 = new JFrame("Escape 1 2 3");//this is the window
         panel1 = new JPanel();//this is the panel that will pop up
+        panel1.setBackground(new Color(209,229,235));
+        escapetitle = new JTextArea("Escape 1 2 3");//text field with no border
+        escapetitle.setEditable(false);//make instructions not be able to edit
         instructions = new JTextArea("Enter your name: ");//text field with no border
         instructions.setEditable(false);//make instructions not be able to edit
-        input = new JTextField(10);//text field with little border
+        input = new JTextField(15);//text field with little border
         input.setEditable(true);//make input editable
+        //input.setForeground(Color.black);
+        //input.setBackground(new Color(150, 176, 183));
         input.addActionListener( new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,8 +43,10 @@ public class GUI extends Main {
         });
         //button.addActionListener( new ad; {public void actionPerformed(ActionEvent e) {})})
         frame1.add(panel1);
-        panel1.add(instructions);
-        panel1.add(input);
+        panel1.setLayout( new GridBagLayout());
+        panel1.add(escapetitle, setConstraints(0, 0, 0.5, 3));
+        panel1.add(instructions, setConstraints(0, 1, 0.5, 1));
+        panel1.add(input, setConstraints(2, 1, 0.5, 2));
 
         frame1.setSize(400,500);
         frame1.setVisible(true);
@@ -46,9 +55,14 @@ public class GUI extends Main {
     public void chooseTimes() {
         frameTimes = new JFrame("Chose your times");
         panelTimes = new JPanel();
+        panelTimes.setBackground(new Color(209,229,235));
         submit = new JButton("Submit!");
+        submit.setForeground(Color.black);
+        submit.setBackground(new Color(150, 176, 183));
         instructions = new JTextArea("Enter the time you want:");
         instructions.setEditable(false);
+        instructions.setForeground(Color.black);
+        instructions.setBackground(new Color(150, 176, 183));
 
         submit.addActionListener( new ActionListener() {
             @Override
@@ -63,11 +77,21 @@ public class GUI extends Main {
 
         frameTimes.setSize(500,600);
         frameTimes.add(panelTimes);
-        panelTimes.add(instructions);
-        panelTimes.add(submit);
-        panelTimes.add(openTimes);
+        panelTimes.setLayout( new GridBagLayout());
+        panelTimes.add(instructions, setConstraints(0, 0, 0.5, 1));
+        panelTimes.add(openTimes, setConstraints(1, 0, 0.5, 1));
+        panelTimes.add(submit, setConstraints(2, 0, 0.5, 1));
 
         frameTimes.setVisible(true);
+    }
+
+    public GridBagConstraints setConstraints(int gridx, int gridy, double weight, int width){
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = gridx;
+        c.weightx = weight;
+        c.gridy = gridy;
+        c.gridwidth = width;
+        return c;
     }
 
     public String[] copyArrayList(ArrayList<String> arrayList) {
